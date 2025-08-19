7b1d5ada6d84b77d055bea92f666b8dffab56e19
abko <abko121115@gmail.com>
2025-08-19 11:47:16 +0900

    feat: add Account entity with auditing fields

---
+++ src/main/java/org/nfactorial/newsfeed/domain/auth/Account.java
@@ -0,0 +1,43 @@
+package org.nfactorial.newsfeed.domain.auth;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account {
+	@Id
+	private Long id;
+
+	// 로그인용
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime modifiedAt;
+
+	// 회원 탈퇴 확인용
+	@Column(nullable = false)
+	private Boolean active = false;
+}
