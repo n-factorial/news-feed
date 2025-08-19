1e666cd7789e5063cb4c1221c2f9b9b04ca75475
seonrizee <seonrizee@gmail.com>
2025-08-19 15:42:09 +0900

    refactor(interaction): rename entity packages and update imports for Follow, Like, and Profile classes

--- src/main/java/org/nfactorial/newsfeed/domain/interaction/entity/Like.java
+++ src/main/java/org/nfactorial/newsfeed/domain/interaction/entity/Like.java
@@ -1,6 +1,13 @@
-package org.nfactorial.newsfeed.domain.interaction.like.entity;
+package org.nfactorial.newsfeed.domain.interaction.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +19,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/nfactorial/newsfeed/domain/profile/entity/Profile.java
+++ src/main/java/org/nfactorial/newsfeed/domain/profile/entity/Profile.java
@@ -1,13 +1,19 @@
 package org.nfactorial.newsfeed.domain.profile.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.nfactorial.newsfeed.domain.auth.entity.Account;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -18,7 +24,7 @@ public class Profile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     private Account account;
 
     @Column(nullable = false, unique = true)
