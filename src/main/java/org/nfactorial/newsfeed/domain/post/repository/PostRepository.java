3004736ce2ddd18a5cc88a0b6ec21dfc4d40fea2
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-20 18:53:36 +0900

    refactor: replace query to PostRepository

--- src/main/java/org/nfactorial/newsfeed/domain/post/repository/PostRepository.java
+++ src/main/java/org/nfactorial/newsfeed/domain/post/repository/PostRepository.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.nfactorial.newsfeed.domain.post.entity.Post;
 import org.nfactorial.newsfeed.domain.profile.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	long countByProfileId(Long profileId);
 
+	@Query("SELECT p.profile.id, COUNT(p) FROM Post p WHERE p.profile IN :profiles GROUP BY p.profile.id")
 	Map<Long, Long> countPostsByProfile(List<Profile> profiles);
 }
--- src/main/java/org/nfactorial/newsfeed/domain/profile/repository/ProfileRepository.java
+++ src/main/java/org/nfactorial/newsfeed/domain/profile/repository/ProfileRepository.java
@@ -1,15 +1,8 @@
 package org.nfactorial.newsfeed.domain.profile.repository;
 
-import java.util.List;
-import java.util.Map;
-
 import org.nfactorial.newsfeed.domain.profile.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 	boolean existsByNickname(String nickname);
-	@Query("SELECT p.profile.id, COUNT(p) FROM Post p WHERE p.profile IN :profiles GROUP BY p.profile.id")
-	Map<Long, Long> countPostsByProfileIn(@Param("profiles") List<Profile> profiles);
 }
