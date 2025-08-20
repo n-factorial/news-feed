56ade4bd8a3df6a9ffdc285df705b9181e07ee40
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-20 15:07:20 +0900

    style: clean up import

--- src/main/java/org/nfactorial/newsfeed/domain/profile/service/ProfileServiceApi.java
+++ src/main/java/org/nfactorial/newsfeed/domain/profile/service/ProfileServiceApi.java
@@ -1,7 +1,6 @@
 package org.nfactorial.newsfeed.domain.profile.service;
 
 import org.nfactorial.newsfeed.domain.profile.dto.request.CreateProfileCommand;
-import org.nfactorial.newsfeed.domain.profile.entity.Profile;
 import org.nfactorial.newsfeed.domain.profile.dto.response.ProfileResponse;
 
 public interface ProfileServiceApi {
