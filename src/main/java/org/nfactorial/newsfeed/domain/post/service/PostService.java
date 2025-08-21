07cdaf36758ad11d94e08f07ed36d0a61acb002c
abko <abko121115@gmail.com>
2025-08-21 19:25:01 +0900

    fix: extract creation & interaction services to fix circular dependency

--- src/main/java/org/nfactorial/newsfeed/domain/post/service/PostService.java
+++ src/main/java/org/nfactorial/newsfeed/domain/post/service/PostService.java
@@ -1,25 +1,17 @@
 package org.nfactorial.newsfeed.domain.post.service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.nfactorial.newsfeed.common.code.ErrorCode;
 import org.nfactorial.newsfeed.common.exception.BusinessException;
 import org.nfactorial.newsfeed.common.security.AuthProfileDto;
 import org.nfactorial.newsfeed.domain.comment.service.CommentServiceApi;
 import org.nfactorial.newsfeed.domain.interaction.service.InteractionQueryServiceApi;
-import org.nfactorial.newsfeed.domain.post.dto.PostCountDto;
-import org.nfactorial.newsfeed.domain.post.dto.request.PostCreateRequest;
 import org.nfactorial.newsfeed.domain.post.dto.request.PostUpdateRequest;
-import org.nfactorial.newsfeed.domain.post.dto.response.PostCreateResponse;
-import org.nfactorial.newsfeed.domain.post.dto.response.PostGetOneResponse;
 import org.nfactorial.newsfeed.domain.post.dto.response.PostUpdateResponse;
 import org.nfactorial.newsfeed.domain.post.entity.Post;
 import org.nfactorial.newsfeed.domain.post.repository.PostRepository;
-import org.nfactorial.newsfeed.domain.profile.entity.Profile;
-import org.nfactorial.newsfeed.domain.profile.service.ProfileServiceApi;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
@@ -31,19 +23,6 @@ import lombok.RequiredArgsConstructor;
 public class PostService implements PostServiceApi {
 
 	private final PostRepository postRepository;
-	private final CommentServiceApi commentService;
-	private final ProfileServiceApi profileService;
-	private final InteractionQueryServiceApi interactionQueryService;
-
-	@Transactional
-	public PostCreateResponse save(PostCreateRequest request, AuthProfileDto currentUserProfile) {
-		long profileId = currentUserProfile.profileId();
-		Profile foundProfile = profileService.getProfileEntityById(profileId);
-
-		Post savedPost = postRepository.save(Post.of(request, foundProfile));
-
-		return PostCreateResponse.of(savedPost);
-	}
 
 	@Transactional
 	public PostUpdateResponse update(Long postId, PostUpdateRequest request,
@@ -58,20 +37,6 @@ public class PostService implements PostServiceApi {
 		foundPost.updateContent(request.content());
 
 		return PostUpdateResponse.of(foundPost);
-	}
-
-	@Transactional
-	public PostGetOneResponse viewPost(Long postId) {
-
-		postRepository.incrementViewCount(postId);
-
-		Post foundPost = getPostById(postId);
-
-		boolean hasLikedPost = interactionQueryService.hasLikedPost(foundPost.getId(), foundPost.getProfile().getId());
-
-		int commentCount = commentService.getCommentCount(foundPost);
-
-		return PostGetOneResponse.of(foundPost, commentCount, hasLikedPost);
 	}
 
 	@Transactional
@@ -127,7 +92,7 @@ public class PostService implements PostServiceApi {
 
 	//TODO: 구현해주세요
 	@Override
-	public Map<Long, Long> countPostsByProfileIds(List<Long> profileIds){
+	public Map<Long, Long> countPostsByProfileIds(List<Long> profileIds) {
 		return null;
 	}
 }
