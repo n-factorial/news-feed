827d34105e7faff9fff218b6deba5eaacffa97e2
abko <abko121115@gmail.com>
2025-08-25 09:22:48 +0900

    refactor: remove unused imports from ProfileServiceApi.

--- src/main/java/org/nfactorial/newsfeed/domain/profile/service/ProfileServiceApi.java
+++ src/main/java/org/nfactorial/newsfeed/domain/profile/service/ProfileServiceApi.java
@@ -3,9 +3,7 @@ package org.nfactorial.newsfeed.domain.profile.service;
 import java.util.List;
 
 import org.nfactorial.newsfeed.domain.profile.dto.request.CreateProfileCommand;
-import org.nfactorial.newsfeed.domain.profile.dto.request.UpdateProfileCommand;
 import org.nfactorial.newsfeed.domain.profile.dto.ProfileSummaryDto;
-import org.nfactorial.newsfeed.domain.profile.dto.response.ProfileResponse;
 import org.nfactorial.newsfeed.domain.profile.entity.Profile;
 
 public interface ProfileServiceApi {
